d3e76c9b3f688192ec3eab1cb038cd5874bc8607
Anastasia <anastalen@gmail.com>
2017-08-14 20:03:01 +0300

    comment

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+
+    {
+        enum Operation
+        {
+            Plus,
+            Minus,
+            Multiplication,
+            Division
+        }
+        static void Main(string[] args)
+        {
+           
+            string[] arr = Numbers();
+            int Var1 = int.Parse(arr[1]);
+            int Var2 = int.Parse(arr[0]);
+            Operation operation = ParseOperation(arr[2]);
+            switch (operation)
+            {
+                case Operation.Plus:
+                    Plus(Var1, Var2);
+                    break;
+                case Operation.Minus:
+                    Minus(Var1, Var2);
+                    break;
+                case Operation.Multiplication:
+                    Multiplication(Var1, Var2);
+                    break;
+                case Operation.Division:
+                    Division(Var1, Var2);
+                    break;
+
+            }
+        }
+
+        static void Plus(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+
+
+        }
+
+        static void Minus(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+
+        }
+        static void Multiplication(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine(c);
+
+        }
+        static void Division(int a, int b)
+        {
+
+            int c = a / b;
+
+            Console.WriteLine(c);
+        }
+        static Operation ParseOperation(string UserValue)
+        {
+            Operation Plus1 = (Operation)Enum.Parse(typeof(Operation), UserValue);
+            return Plus1;
+        }
+        static string[] Numbers()
+        {
+            string[] mas = new string[3];
+            
+            Console.WriteLine("Please enter number1");
+            mas[0]= Console.ReadLine();
+            Console.WriteLine("Please enter number2");
+            mas[1] = Console.ReadLine();
+            Console.WriteLine("Please enter operation");
+            mas[2] = Console.ReadLine();
+            return mas;
+        }
+
+
+
+    }
+}
---
+++ Program_1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_do_while_Гаевая_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /* Повторение:
+            1.Пользователь вводит число. Вывести факториал числа.
+            Факториал числа представляет собой произведение всех натуральных чисел от 1 до этого числа включительно. 
+            Console.WriteLine("Please enter number");
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for(int i=number;i>1;i--)
+            {
+                factorial *= i;               
+            }
+            Console.WriteLine(factorial);
+            2.Найти индексы одинаковых значений 2 - х разных масовов. массив задается пользователем любым удобным для вас способом.
+            int[] nums1 = new int[4];
+            int[] nums2 = new int[4];
+            for (int i=0;i<4;i++)
+            {
+                Console.WriteLine("Please input number"+(i + 1)+ "from first array");
+                nums1[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please input number" + (i + 1) + "from second array");
+                nums2[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=0;i<4;i++)
+                if(nums1[i]==nums2[i])
+                {
+                    Console.WriteLine(i + 1);
+                }
+           // 3.Написать калькулятор с возможными операциями(+,-,/,*).
+            
+            Console.WriteLine("Please enter number1");
+            int Var1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number2");
+            int Var2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter operation");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "plus":
+                    Plus(Var1, Var2);
+                    break;
+                case "minus":
+                    Minus(Var1, Var2);
+                    break;
+                case "multiplication":
+                    Multiplication(Var1, Var2);
+                    break;
+                case "division":
+                    Division(Var1, Var2);
+                    break;
+
+            }
+        }
+
+        static void Plus(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+
+
+        }
+
+        static void Minus(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+
+        }
+        static void Multiplication(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine(c);
+
+        }
+        static void Division(int a, int b)
+        {
+           
+            
+                int c = a / b;
+               Console.WriteLine(c);
+        }
+
+
+
+            
+           // While /do while
+         // 1.Пользователь должен вводить числа по очереди, до тех пор пока разница введеных двух предыдущих чисел не превысит 10.*/
+
+            while(int n - (n - 1) > 10);
+            {
+                Console.WriteLine("Enter number:  ");
+                 n = int.Parse(Console.ReadLine());
+            }
+     
+
+        }   
+    }
+}
